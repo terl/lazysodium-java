e8f8ba54e44ad9a264a3b4d7b8a6337a0af1b826
Gurpreet Paul <gurpreet@terl.co>
2018-11-19 22:18:22 +0000

    All 64

--- src/main/java/com/goterl/lazycode/lazysodium/SodiumJava.java
+++ src/main/java/com/goterl/lazycode/lazysodium/SodiumJava.java
@@ -174,7 +174,7 @@ public class SodiumJava extends Sodium {
             if (is64Bit) {
                 path = getPath("linux64", "libsodium.so");
             } else {
-                path = getPath("linux", "libsodium.so");
+                path = getPath("linux64", "libsodium.so");
             }
         } else if (Platform.isMac()) {
             path = getPath("mac", "libsodium.dylib");
